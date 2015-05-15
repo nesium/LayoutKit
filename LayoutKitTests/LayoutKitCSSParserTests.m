7ad67676ad635d5895fefe0553c04f166f2fc30a
mb <mb@nesium.com>
2015-05-16 00:35:28 +0200

    disabled test

--- LayoutKitTests/LayoutKitCSSParserTests.m
+++ LayoutKitTests/LayoutKitCSSParserTests.m
@@ -9,24 +9,24 @@
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
-@import LayoutKitCSSParser.LYKCSSDocument;
+//@import LayoutKitCSSParser.LYKCSSDocument;
 
 @interface LayoutKitCSSParserTests : XCTestCase
 @end
 
-LYKCSSDocument *CSSDocumentWithFile(NSString *filename)
-{
-    NSBundle *bundle = [NSBundle bundleForClass:[LayoutKitCSSParserTests class]];
-    NSLog(@"%@", bundle);
-    NSString *path = [bundle pathForResource:filename ofType:@"css" inDirectory:@"testfiles"];
-    
-    return [[LYKCSSDocument alloc] initWithContentsOfFile:path error:nil];
-}
+//LYKCSSDocument *CSSDocumentWithFile(NSString *filename)
+//{
+//    NSBundle *bundle = [NSBundle bundleForClass:[LayoutKitCSSParserTests class]];
+//    NSLog(@"%@", bundle);
+//    NSString *path = [bundle pathForResource:filename ofType:@"css" inDirectory:@"testfiles"];
+//    
+//    return [[LYKCSSDocument alloc] initWithContentsOfFile:path error:nil];
+//}
 
 @implementation LayoutKitCSSParserTests
 
-- (void)testExample {
-    LYKCSSDocument *doc = CSSDocumentWithFile(@"1_simple");
-    [doc parse:nil];
-}
+//- (void)testExample {
+//    LYKCSSDocument *doc = CSSDocumentWithFile(@"1_simple");
+//    [doc parse:nil];
+//}
 @end
